3a9a9e613843701926090facdf06c087ad070ad9
Riteshk1314 <132162137+Riteshk1314@users.noreply.github.com>
2024-01-12 14:59:55 +0000

    Pending changes exported from your codespace

---
+++ JSwithHitesh/BasicsOfJs/basic02.js
@@ -0,0 +1,16 @@
+ const accountId= 10 //can not be changed 
+ let accountEmail= "riteshkapoor1314@gmail.com" 
+ var accountPassword= "1234" //can be changed
+ accountCity = "jaipur"
+
+ accountEmail= "hchc@hc.org"
+ accountPassword="121212"
+ accountCity= "pune"
+ //accountId=11 not allowed
+/*
+prefer not to use var 
+beacuse of issue in block scope and functional scope 
+
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
