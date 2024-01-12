9545118d053d9f89406fa56408adb4583819d907
Riteshk1314 <132162137+Riteshk1314@users.noreply.github.com>
2024-01-12 20:43:56 +0530

    Add files via upload

---
+++ JSwithHitesh/BasicsOfJs/datatype&ECMA.js
@@ -0,0 +1,26 @@
+
+
+
+"use strict";
+//alert(3+3) we are using node.js not browser
+
+//generally we avoid ; in js 
+
+console.log(3+3 ); console.log( "hello ")
+
+let name= "hellooo"
+let age = 19
+let isStudent = true
+
+//number => 2^53-1
+//big int
+// string " "
+//boolean => true/false
+//null => standalone value.. basically nothing
+//undefined => value is not defined 
+
+//object 
+console.log(typeof null)
+// null is an  object 
+console.log(undefined)
+//undefined 
