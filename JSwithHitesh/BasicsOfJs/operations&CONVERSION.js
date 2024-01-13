ab1d0ee0281fac356069b1293d842612607692bf
Riteshk1314 <132162137+Riteshk1314@users.noreply.github.com>
2024-01-13 15:29:16 +0530

    Add files via upload

---
+++ JSwithHitesh/BasicsOfJs/operations&CONVERSION.js
@@ -0,0 +1,34 @@
+
+//conversion and operations 
+
+// let score = 22
+// let score2="22abc" //or if we wrote 22.5 it will be a number or if we wrote this only then we get NaN but its type is still number.... true or false will give 1 and 0  
+// console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof score2)
+// console.log(typeof(score2))
+// let valueInNumber= Number(score2)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+//*********operations**********
+
+let value = 3
+let negativeval= -value 
+console.log(negativeval)
+
+let str1= "helllo"
+let str2= " world"
+console.log(str1+str2)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(1+2+2+"1")
+console.log(true)
+console.log(true+1)
+console.log(+true)
+console.log(+"")
+
