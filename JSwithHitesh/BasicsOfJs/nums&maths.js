dcc125857c22590f6e90a1b78c873078cf857459
Riteshk1314 <132162137+Riteshk1314@users.noreply.github.com>
2024-01-19 14:16:45 +0530

    Add files via upload

---
+++ JSwithHitesh/BasicsOfJs/nums&maths.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score)
+const score1 = new Number(400)
+console.loga(score1)
+console.log(score1.toString())
+console.log(score1.valueOf())
+console.log(score1.toFixed(2))
+consloe.log(score1.toPrecision(2))
+const othernumber= 23.777
+console.log(othernumber.toPrecision(2))//returna a string 
+
+const hnderds = 20000000
+console.log(hnderds.toLocaleString("en-IN"))
+let anumb= number.MAX_VALUE
+
+
+//******maths******
+const num1 = 3.14
+console.log(Maths.abs(-4))
+console.log(Maths.round(num1))
+console.log(Maths.ceil(num1))
+console.log(Math.floor(num1))
+console.log(Math.min(3,4,6,6,322,2))
+console.log(Math.random())// 0 to 1 
+
+console.log((Math.random()*10)+1)// 1 to 10
+
+const min = 19
+const max = 100
+console.log(Math.floor(Math.random()*(max-min+1)+min))
---
+++ JSwithHitesh/BasicsOfJs/strings.js
@@ -0,0 +1,16 @@
+const name = "ritesh"
+const repocount= 50
+console.log(name + repocount+"value" )//not a great syntax
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+//this is a better syntax
+const gameName = new String("mario")
+console.log(gameName[0])
+console.log(gameName.__proto__.lenght)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+console.log(gameName.charAt(0))
+console.log(gameName.indexOf("r"))
+const newString = gameName.replace("mario","luigi")
+co
